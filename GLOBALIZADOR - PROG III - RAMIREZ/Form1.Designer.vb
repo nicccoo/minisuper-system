5330bb90270162ceb8da71772d47d22774fadbc4
Nicolas <nicolas_31_07@hotmail.com>
2021-04-30 11:11:55 -0300

    done

---
+++ GLOBALIZADOR - PROG III - RAMIREZ/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtProducto = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.cmbUnidadM = New System.Windows.Forms.ComboBox()
+        Me.cmbTipoP = New System.Windows.Forms.ComboBox()
+        Me.dtpFechaL = New System.Windows.Forms.DateTimePicker()
+        Me.btnRegistrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.mskCodigo = New System.Windows.Forms.MaskedTextBox()
+        Me.SuspendLayout()
+        '
+        'txtProducto
+        '
+        Me.txtProducto.Location = New System.Drawing.Point(219, 127)
+        Me.txtProducto.Name = "txtProducto"
+        Me.txtProducto.Size = New System.Drawing.Size(100, 20)
+        Me.txtProducto.TabIndex = 1
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(219, 194)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 2
+        '
+        'cmbUnidadM
+        '
+        Me.cmbUnidadM.FormattingEnabled = True
+        Me.cmbUnidadM.Location = New System.Drawing.Point(219, 274)
+        Me.cmbUnidadM.Name = "cmbUnidadM"
+        Me.cmbUnidadM.Size = New System.Drawing.Size(121, 21)
+        Me.cmbUnidadM.TabIndex = 3
+        '
+        'cmbTipoP
+        '
+        Me.cmbTipoP.FormattingEnabled = True
+        Me.cmbTipoP.Location = New System.Drawing.Point(219, 163)
+        Me.cmbTipoP.Name = "cmbTipoP"
+        Me.cmbTipoP.Size = New System.Drawing.Size(121, 21)
+        Me.cmbTipoP.TabIndex = 4
+        '
+        'dtpFechaL
+        '
+        Me.dtpFechaL.Location = New System.Drawing.Point(219, 234)
+        Me.dtpFechaL.Name = "dtpFechaL"
+        Me.dtpFechaL.Size = New System.Drawing.Size(200, 20)
+        Me.dtpFechaL.TabIndex = 5
+        '
+        'btnRegistrar
+        '
+        Me.btnRegistrar.Location = New System.Drawing.Point(289, 328)
+        Me.btnRegistrar.Name = "btnRegistrar"
+        Me.btnRegistrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnRegistrar.TabIndex = 6
+        Me.btnRegistrar.Text = "Registrar"
+        Me.btnRegistrar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(153, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Codigo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(156, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Producto"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(118, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Tipo de producto"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(124, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(82, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Precio de venta"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(102, 234)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(111, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Fecha de lanzamiento"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(119, 277)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(93, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Unidad de medida"
+        '
+        'mskCodigo
+        '
+        Me.mskCodigo.Location = New System.Drawing.Point(219, 93)
+        Me.mskCodigo.Mask = "?0000"
+        Me.mskCodigo.Name = "mskCodigo"
+        Me.mskCodigo.Size = New System.Drawing.Size(135, 20)
+        Me.mskCodigo.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.mskCodigo)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRegistrar)
+        Me.Controls.Add(Me.dtpFechaL)
+        Me.Controls.Add(Me.cmbTipoP)
+        Me.Controls.Add(Me.cmbUnidadM)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.txtProducto)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtProducto As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents cmbUnidadM As ComboBox
+    Friend WithEvents cmbTipoP As ComboBox
+    Friend WithEvents dtpFechaL As DateTimePicker
+    Friend WithEvents btnRegistrar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents mskCodigo As MaskedTextBox
+End Class
---
+++ GLOBALIZADOR - PROG III - RAMIREZ/Form1.vb
@@ -0,0 +1,123 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+
+
+
+        Dim CadenaSQL As String
+        Dim Tabla As DataTable
+
+        CadenaSQL = "SELECT * FROM Productos Order by Producto asc"
+        Tabla = New DataTable
+        Recuperar(CadenaSQL, Tabla)
+
+        cmbTipoP.DataSource = Tabla
+        cmbTipoP.DisplayMember = "Producto"
+        cmbTipoP.ValueMember = "IdTipoDeProducto"
+        cmbTipoP.DropDownStyle = ComboBoxStyle.DropDownList
+        cmbTipoP.SelectedIndex = 0
+
+        Dim Archivo As FileStream
+        Dim Lector As StreamReader
+        Dim linea As String
+
+
+
+        Archivo = New FileStream("Unidades.txt", FileMode.Open)
+        Lector = New StreamReader(Archivo)
+
+        While Lector.EndOfStream = False
+
+            linea = Lector.ReadLine()
+
+            With cmbUnidadM
+
+                cmbUnidadM.Items.Add(linea)
+
+            End With
+
+        End While
+        cmbUnidadM.DropDownStyle = ComboBoxStyle.DropDownList
+        cmbUnidadM.SelectedIndex = 0
+
+
+        txtProducto.MaxLength = 30
+
+        mskCodigo.MaxLength = 5 'PARA UTILIZARLA DEBE SELECCIONAR AL PRINCIPIO DEL MASKEDBOX
+        dtpFechaL.Format = DateTimePickerFormat.Custom
+        dtpFechaL.CustomFormat = "dd/MM/yy"
+        dtpFechaL.MaxDate = DateTime.Today
+
+
+    End Sub
+
+    Private Function Validar() As Boolean
+        Dim CadenaSQL As String
+        Dim Tabla As DataTable
+        Dim TodoOk As Boolean
+        Dim Numero As Integer
+
+        TodoOk = False
+
+        If txtPrecio.Text.Trim <> String.Empty Then
+            If Integer.TryParse(txtPrecio.Text, Numero) Then
+                If Numero > 0 Then
+                    CadenaSQL = "Select * from Productos where Codigo ='" & mskCodigo.Text.Trim & "'"
+                    Tabla = New DataTable
+                    Recuperar(CadenaSQL, Tabla)
+                    If Tabla.Rows.Count = 0 Then
+                        TodoOk = True
+                    Else
+                        MessageBox.Show("El codigo ingresado se encuentra asignado", "ATENCION!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    End If
+
+                Else
+                    MessageBox.Show("El precio debe ser mayor a 0", "ATENCION!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                End If
+
+            Else
+                MessageBox.Show("El precio debe ser numerico", "ATENCION!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Else
+            MessageBox.Show("No ingreso precio", "ATENCION!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+        Return TodoOk
+    End Function
+
+    Private Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
+        Dim CadenaSQL As String
+
+        If Validar() Then
+
+            CadenaSQL = "INSERT INTO Productos(Codigo, Producto, IdTipoDeProducto, PrecioVenta, FechaLanzamiento, UnidadMedida)"
+            CadenaSQL = CadenaSQL & " VALUES('" & mskCodigo.Text.Trim & "','" & txtProducto.Text.Trim & "'," & cmbTipoP.SelectedValue & ","
+            CadenaSQL = CadenaSQL & txtPrecio.Text.Trim & ",'" & dtpFechaL.Value.ToShortDateString & "','" & cmbUnidadM.SelectedValue & "')"
+
+
+            Try
+
+                Ejecutar(CadenaSQL)
+                MessageBox.Show("Producto registrado!", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                mskCodigo.Text = String.Empty
+                txtPrecio.Text = String.Empty
+                txtProducto.Text = String.Empty
+                cmbTipoP.Focus()
+
+            Catch ex As Exception
+
+                MessageBox.Show("La operación fallida: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+            End Try
+
+
+        End If
+
+    End Sub
+
+    Private Sub mskCodigo_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles mskCodigo.MaskInputRejected
+        Me.Focus()
+    End Sub
+End Class
